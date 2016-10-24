9237c6040709191968aa44630e5a60ac565d4b87
unknown <35527@ICF2006302.ICFI.icfconsulting.com>
2016-10-24 16:23:51 -0400

    Added mongo support

--- controllers/fbbotcontroller.js
+++ controllers/fbbotcontroller.js
@@ -17,17 +17,17 @@ var bot = controller.spawn({
 
 // Triggered by send-to-messenger plugin
 controller.on('facebook_optin', function (bot, message) {
-  bot.reply(message, 'Welcome, friend')
+  bot.reply(message, 'Welcome, friend');
 });
 
 // User sends greetings
 controller.hears(['hello', 'hi', 'hey'], 'message_received', function (bot, message) {
-  bot.reply(message, 'Hey there.')
+  bot.reply(message, 'Hey there.');
 });
 
 // User says anything else
 controller.hears('(.*)', 'message_received', function (bot, message) {
-  bot.reply(message, 'you said ' + message.match[1])
+  bot.reply(message, 'you said ' + message.match[1]);
 });
 
 // Facebook webhook handler
