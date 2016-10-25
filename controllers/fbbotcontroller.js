a0fb5cdc0e4421d37f4a637a1f64c05485e7a43b
unknown <35527@ICF2006302.ICFI.icfconsulting.com>
2016-10-25 01:41:18 -0400

    Bugfix

--- controllers/fbbotcontroller.js
+++ controllers/fbbotcontroller.js
@@ -69,7 +69,7 @@ controller.hears(['^(?!postback)$', 'comic'], 'message_received', function(bot, 
 // User wants to conversation history
 controller.hears(['conversation'], 'message_received', function(bot, message) {
     //bot.reply(message, getComic());
-    getUserData(message.user);
+    getUserData(message.channel);
 });
 
 
@@ -234,7 +234,7 @@ var createUser = function(id, timestamp) {
 }
 
 var getUserData = function(id) {
-	controller.storage.users.get(id, function(err, userData) {
+	controller.storage.channels.get(id, function(err, userData) {
 		console.log(JSON.stringify(userData));
 	});
 }
