93285b1c0887db550cdfb965032a834560f35e02
unknown <35527@ICF2006302.ICFI.icfconsulting.com>
2016-10-24 23:01:32 -0400

    Bugfix

--- controllers/fbbotcontroller.js
+++ controllers/fbbotcontroller.js
@@ -167,8 +167,8 @@ var createButtonMessage = function() {
 }
 
 var getComic = function() {
-	var randComic = numberOfComicsAvailable * Math.random() + 1;
-	var randomComicUrl = 'http://xkcd.com/' + randComic + '/info.0.json';
+	var randComic = Math.ceil(numberOfComicsAvailable * Math.random()) + 1;
+	var randomComicUrl = 'http://xkcd.com/' + randComic)+ '/info.0.json';
 	console.log('rand comic url ' + randomComicUrl);
 	request('http://www.google.com', function (error, response, body) {
   	if (!error && response.statusCode == 200) {
